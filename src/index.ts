e4c2d8a6b8317bc51b15a7fac8c727fe9212bffa
ecc97 <carmonaedwin1123@gmail.com>
2024-07-18 20:03:44 -0500

    fix: create user

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { RequestLogin, ResponseLogin } from "./models/auth.model"
+import { ResponseCreateUser, RequestCreateUser } from "./models/users.model"
 import { AuthController } from "./controllers/auth.controller"
+import { UsersController } from "./controllers/users.controller"
 
 async function main(): Promise<void>{
     const dataToLogin: RequestLogin = {
@@ -7,13 +9,24 @@ async function main(): Promise<void>{
         password: 'C0ntr4S3gu++r4'
     }
 
+    const userToRegister: RequestCreateUser = {
+        name: 'Yas',
+        lastName: 'Muñoz',
+        email: 'yaima@mail.com',
+        password: 'password78'
+    }
+
     const authController: AuthController = new AuthController()
+    const usersController: UsersController = new UsersController()
 
     try {
        const resultLogin: ResponseLogin = await authController.postLogin(dataToLogin)
        console.log(resultLogin)
        const token = resultLogin.data.token
        console.log('Token:', token)
+
+       const createUserResponse: ResponseCreateUser = await usersController.createUser(userToRegister)
+       console.log(createUserResponse)
     } catch (error) {
         console.error(`=( : ${error}`)
     }
